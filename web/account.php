61a9f726765403c3c67b3570a10d772d15ecc15a
smrkhan123 <sk862147@gmail.com>
2020-12-07 20:22:14 +0530

    task added

---
+++ web/account.php
@@ -0,0 +1,54 @@
+<?php include('header.php'); ?>
+		<!---login--->
+			<div class="content">
+				<!-- registration -->
+	<div class="main-1">
+		<div class="container">
+			<div class="register">
+		  	  <form> 
+				 <div class="register-top-grid">
+					<h3>personal information</h3>
+					 <div>
+						<span>First Name<label>*</label></span>
+						<input type="text"> 
+					 </div>
+					 <div>
+						<span>Last Name<label>*</label></span>
+						<input type="text"> 
+					 </div>
+					 <div>
+						 <span>Email Address<label>*</label></span>
+						 <input type="text"> 
+					 </div>
+					 <div class="clearfix"> </div>
+					   <a class="news-letter" href="#">
+						 <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up for Newsletter</label>
+					   </a>
+					 </div>
+				     <div class="register-bottom-grid">
+						    <h3>login information</h3>
+							 <div>
+								<span>Password<label>*</label></span>
+								<input type="password">
+							 </div>
+							 <div>
+								<span>Confirm Password<label>*</label></span>
+								<input type="password">
+							 </div>
+					 </div>
+				</form>
+				<div class="clearfix"> </div>
+				<div class="register-but">
+				   <form>
+					   <input type="submit" value="submit">
+					   <div class="clearfix"> </div>
+				   </form>
+				</div>
+		   </div>
+		 </div>
+	</div>
+<!-- registration -->
+
+			</div>
+<!-- login -->
+<?php include('footer.php'); ?>
